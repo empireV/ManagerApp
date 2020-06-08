b6ccd9e9cb9954849988ebab61249de7052e950a
empireV <vovaempire@gmail.com>
2020-06-08 17:57:43 +0300

    started to create com onents

---
+++ client/manager-app/src/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './Header.scss';
+
+const CN = 'app-header'
+export const Header = (props) => {
+    return (
+        <div className={CN}>
+
+        </div>
+    )
+}
