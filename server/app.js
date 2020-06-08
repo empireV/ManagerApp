19a521c15c276128a2357b3bf43fc4848d0ef4d1
Andrei Prykhodko <whiterandrek@gmail.com>
2020-06-08 18:11:01 +0300

    SERVER: Listen to requests on port 8081

--- server/app.js
+++ server/app.js
@@ -1,9 +1,6 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -11,10 +8,10 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var listener = app.listen(process.env.PORT || 8081, 
+    () => console.log(`Server start on port ${listener.address().port} ...`))
 
 module.exports = app;
