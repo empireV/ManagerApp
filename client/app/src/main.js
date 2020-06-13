3b08e0e380b0e157d2e03aca96b19e4e5c2bca4f
Andrei Prykhodko <whiterandrek@gmail.com>
2020-06-13 23:04:46 +0300

    init axios base URL

--- client/app/src/main.js
+++ client/app/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://localhost:8081/'
 
 new Vue({
   render: h => h(App),
